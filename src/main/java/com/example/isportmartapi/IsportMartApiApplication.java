b76216cc9bf59f1817d0747f6e7e02094742cc77
mahao <peter8icestone@gmail.com>
2022-06-25 22:48:04 +0800

    init

---
+++ src/main/java/com/example/isportmartapi/IsportMartApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.isportmartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsportMartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IsportMartApiApplication.class, args);
+    }
+
+}
