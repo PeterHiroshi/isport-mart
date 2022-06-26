459df813d50449f5b1638edb4aa74a5437c41deb
mahao <peter8icestone@gmail.com>
2022-06-26 19:44:15 +0800

    添加商品分页展示api

--- src/main/java/com/example/isportmartapi/entity/Product.java
+++ src/main/java/com/example/isportmartapi/entity/Product.java
@@ -1,14 +1,44 @@
 package com.example.isportmartapi.entity;
 
-import lombok.extern.slf4j.Slf4j;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
 
 import java.io.Serializable;
 
 /**
  * @author mahao
- * @since 2022-06-25
+ * @since 2022-06-26
  */
-@Slf4j
+@Data
+@TableName("`product`")
 public class Product implements Serializable {
-    private static final long serialVersionUID = 7728275135274822570L;
+
+    private static final long serialVersionUID = 795549075446940051L;
+
+    @TableId(value = "`id`", type = IdType.AUTO)
+    private Integer id;
+
+    @TableField("`productName`")
+    private String productName;
+
+    @TableField("`productPrice`")
+    private Double productPrice;
+
+    @TableField("`count`")
+    private Integer count;
+
+    @TableField("`productLogo`")
+    private String productLogo;
+
+    @TableField("`productCity`")
+    private String productCity;
+
+    @TableField("`classId`")
+    private Integer classId;
+
+    @TableField("`viewCount`")
+    private Integer viewCount;
 }
